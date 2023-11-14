7bf0a4160a5bc501680e700086f88ce8cd3733d6
EmiroMunoz01 <emiro_2001@icloud.com>
2023-11-14 10:51:42 -0500

    seguiremos estudiando API Fetch

--- app/public/register.js
+++ app/public/register.js
@@ -1,4 +1,17 @@
-document.getElementById("register-form").addEventListener("submit", (e) => {
-    console.log(e);
-});
-console.log("register js");
+document
+  .getElementById("register-form")
+  .addEventListener("submit", async (e) => {
+    e.preventDefault();
+    console.log(e.target.children.user.value);
+
+    //nos comunicaremos con el backend para hacer le registro, para esto usaremos fetch, cuando usamos fetch sabemos que usamos funciones asincronicas, donde empieza la funcion flecha usaremos async
+
+    const res = await fetch("http://localhost:4000/api/register", {
+      //tendra un objeto de opciones
+      method: "POST",
+      headers:{
+        //lo que enviaremos es de tipo JSON, por eso debe tener
+        "Content-Type":"application/json"
+      }
+    });
+  });
